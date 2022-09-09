7ca7a8aa36d6a27a388f55676e472edc0d6458fe
m6-zamani <emmygguy@gmail.com>
2022-09-08 18:35:24 -0700

    Task 4 Level C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Printing in C language
- *
- * Return Always 0 (Successful)
+ * main - printing in C programming
+ * 
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
