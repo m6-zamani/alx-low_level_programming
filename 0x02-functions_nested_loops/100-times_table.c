46d1e9df82a3070a5c3722f15f237ad4605e2a66
m6-zamani <emmygguy@gmail.com>
2022-09-14 16:59:09 -0700

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n, i;
+
+	for (i = 0; i <= 15; ++i)
+	{
+		printf("%d * %d = %d \n", n, i, n * i);
+	}
+
+	return (0);
+}
