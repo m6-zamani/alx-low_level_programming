47768e43581c94913e934ab7944ffe26d840d746
m6-zamani <emmygguy@gmail.com>
2022-09-23 18:58:24 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,24 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * strcat - concatenates the string pointed to be @src, including the terminal null byte, too the end of the string pointed to by @dest
- * @dest: A pointer to the string to be concatenated upon
- * @src: The source string to be appended to @dest
- *
- *Return: & pointer to the destintion string @dest
+ * _strct -> this is a function strcat
+ * @dest: first param
+ * @src: second param
+ * Return: a string
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, destl_len = 0;
+	int len = 0, i;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	while (dest[len])
+		len++;
+ 
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
