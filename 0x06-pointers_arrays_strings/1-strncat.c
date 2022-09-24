94077417b17f7395f1f67c8d8d4a754974b5e758
m6-zamani <emmygguy@gmail.com>
2022-09-23 19:19:18 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,12 @@
 #include <string.h>
 
 /**
- * strcat - Concatenates the tsring pointed to by @srx, including th terminate nullbyte, to the end of the string pointed to by @dest
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * strncat -> function to append some char x
+ * @dest: first param
+ * @src: second param
+ * @n: 3rd param
  *
- * Return: A pointer to the destination string @dest.
+ * Return: string
  */
 char *_strncat(char *dest, char *src, int n);
 {
