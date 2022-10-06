fc2e4c476177fd24bb4e60dcd88997d839174f10
m6-zamani <emmygguy@gmail.com>
2022-10-05 19:29:02 -0700

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: length of argv
+ * @argv: number of argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	position = total = change = aux = 0;
+
+	if (argc != 2)
+	{
+		printf("@Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]);
+
+	if (total <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+}
