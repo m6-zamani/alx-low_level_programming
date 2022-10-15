abf825f14ac20b6fff1edd1c9d5b06eedb4363e8
m6-zamani <emmygguy@gmail.com>
2022-10-14 20:25:02 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,8 +3,9 @@
 #include <stdarg.h>
 
 /**
+ * separator - The string to be separated
  * print_numbers -Prints numbers, followed by a new line
- * @separator; The string to be printed between numbers.
+ * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function
  * @...: A variable number of numbers to be printed.
  */
