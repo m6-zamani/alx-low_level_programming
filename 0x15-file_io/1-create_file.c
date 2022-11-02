7dbabb4fc7066a8f05296551f9084187b15fe8ce
m6-zamani <emmygguy@gmail.com>
2022-11-01 17:45:56 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * create_file - Entry Point
@@ -13,20 +13,25 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 		return (-1);
+
 	while (text_content[i])
 	i++;
+
 	if (text_content == NULL)
 	{
 		close(file);
 		return (-1);
 	}
+
 	else
 	{
 		write(file, text_content, i);
 	}
+
 	close(file);
 	return (1);
 }
